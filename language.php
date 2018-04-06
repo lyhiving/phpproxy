d1b58dbca35eebcb03655b22770f185479d1d32b
lyhiving <lyhiving@gmail.com>
2018-04-06 17:51:11 +0800

    初始化

---
+++ language.php
@@ -0,0 +1,9 @@
+<?php
+$lang["slogan"] = "了无边界，回归初心";
+$lang["wikipedia"] = "百科";
+$lang["back"] = "返回";
+$lang["go"] = "Go";
+$lang["home"] = "首页";
+$lang["tos"] = "不作恶，不废话";
+$lang["agree"] = "使用本站即表明你同意本";
+$lang["tos_2"] = "使用协议";
